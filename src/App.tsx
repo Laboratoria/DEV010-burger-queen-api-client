b30d8cf70c5223b6dd69c85d1a81786cefae0266
unknown <ginapedraza00@gmail.com>
2023-11-14 17:15:27 -0300

    vista login maqueta

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Login from './components/Login'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
+    <Login />
     </>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,19 @@
+import Logo from '../assets/burger-queen-logo.png';
 
 
 const Login = () => {
 return (
-    <section className="login-section">
-        
+    <section className='login-section'>
+        <section className='login-container'>
+            <img className = 'logo-img' src={Logo} />
+            <h1>Bienvenido/a</h1>
+            <form>
+                <input type='email' placeholder='Email' />
+                <input type='password' placeholder='Contraseña'/>
+                <button className='login-button'>Ingresar</button>
+            </form>
+
+        </section>
     </section>
 )
 
