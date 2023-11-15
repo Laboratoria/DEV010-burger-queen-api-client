aef1c987d0a597b926ac61bb1ce84801271db4ed
unknown <ginapedraza00@gmail.com>
2023-11-15 18:08:47 -0300

    Errores en configuracion de test

--- src/components/Login/login.test.tsx
+++ src/components/Login/login.test.tsx
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Login from "./Login";
+import { fireEvent, render } from '@testing-library/react';
+import { auth } from "../../services/request";
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: jest.fn()
 }));
+
+describe('Login component', () => {
+it('should navigate to /waiter/newOrder when a waiter is logged in with a correct accessToken', () => {
+  const email = 'waiter@systers.xyz';
+  const password = '123456'
+
+  const {getByText} = render(<Login />);
+  fireEvent.click(getByText('Ingresar'));
+  expect(auth).toHaveBeenCalledWith(email, password);
+  expect(useNavigate).toHaveBeenCalledWith("/waiter/newOrder");
+
+}) 
+
+});
