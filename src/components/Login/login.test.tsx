dae9e5af7b285364172b4dc203983909c2831b64
n-rogget <nicole.rogget@gmail.com>
2023-11-16 18:10:49 -0300

    dos test pasando

--- src/components/Login/login.test.tsx
+++ src/components/Login/login.test.tsx
@@ -2,14 +2,16 @@ import Login from "./Login";
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { auth } from "../../services/request";
 import '@testing-library/jest-dom';
+import { Token } from "../../types/Types";
 
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: jest.fn(() => jest.fn())
 }));
+
 jest.mock("../../services/request", () => ({
-  auth: jest.fn(() => Promise.resolve({
+  auth: jest.fn(():Promise < Token | string >  => Promise.resolve({
     accessToken: " ",
     user: {
       email: 'waiter@systers.xyz',
@@ -31,24 +33,29 @@ describe('Login component', () => {
     });
   });
 
-    it('should display error message for missing email and password', async () => {
-      const { getByText, getByPlaceholderText } = render(<Login />);
+  it('should display error message for missing email and password', async () => {
+    //console.log(auth)
 
-         // Simular el ingreso de datos incorrectos
+    (auth as jest.Mock).mockResolvedValueOnce('Incorrect password')
+
+
+    const { getByText, getByPlaceholderText } = render(<Login />);
+
+    // Simular el ingreso de datos incorrectos
     fireEvent.change(getByPlaceholderText('Email'), {
       target: { value: 'test@example.com' },
     });
     fireEvent.change(getByPlaceholderText('Contraseña'), {
       target: { value: 'incorrectPassword' },
     });
-  
-      // Simular el clic en el botón de ingresar sin proporcionar email y contraseña
-      fireEvent.click(getByText('Ingresar'));
-  
-      // Esperar a que se renderice el mensaje de error
-      await waitFor(() => {
-        expect(getByText('Contraseña incorrecta')).toBeInTheDocument();
-      });
+
+    // Simular el clic en el botón de ingresar sin proporcionar email y contraseña
+    fireEvent.click(getByText('Ingresar'));
+
+    // Esperar a que se renderice el mensaje de error
+    await waitFor(() => {
+      expect(getByText('Contraseña incorrecta')).toBeInTheDocument();
     });
-  
   });
+
+});
--- src/services/request.ts
+++ src/services/request.ts
@@ -3,25 +3,25 @@ import { Token } from "../types/Types";
 
 
 export const auth = async (email: string, password: string): Promise<Token> => {
-    const response = await fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept-Encoding': 'gzip, deflate, br',
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password,
-        }),
-    })
+  const response = await fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept-Encoding': 'gzip, deflate, br',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
 
-    try {     
-        const data: Token = await response.json();
-        return data;
-    } catch (error) {
-        const errorText = await response.text(); 
-        // Manejar cualquier error durante la solicitud
-        console.error('Error during authentication:', errorText);
-        throw error;
-    }
-};
+  /*     try {     */
+  const data: Token = await response.json();
+  return data;
+  /* } catch (error) {
+      const errorText = await response.text(); 
+      // Manejar cualquier error durante la solicitud
+      console.error('Error during authentication:', errorText);
+      throw error;
+  } */
+}; 
