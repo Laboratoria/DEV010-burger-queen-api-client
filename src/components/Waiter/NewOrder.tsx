21aab2fbcdb2065e00d3308f23041ed62f2bc720
n-rogget <nicole.rogget@gmail.com>
2023-11-17 13:03:26 -0300

    Creacion de vista waiter/neworder y comienzo de estilos de la vista

--- src/components/Waiter/NewOrder.tsx
+++ src/components/Waiter/NewOrder.tsx
@@ -1,7 +1,67 @@
-
+import Logo from '../../assets/burger-queen-logo.png';
+import LogOut from '../../assets/log-out.png';
+import Profile from '../../assets/profile.png';
 
 const NewOrder = () => {
-return <p>Acá nueva orden</p>
+  return (
+    <section className='newOrder-Section'>
+      <section className='headerSection'>
+        <img className='logo-img' id='logo-Header' src={Logo} />
+        <section className='buttonHeader'>
+        <button id='profile-button' className='headerButton'>
+          Role
+          <img className='button-img' id='profileImg' src={Profile} />
+        </button>
+        <button id='logOut-button' className='headerButton'>
+          <img className='button-img' id='logOutimg' src={LogOut} />
+        </button>
+        </section>
+      </section>
+      <section className='orderSection'>
+        <button className='allOrdersButton'> Ver todos los pedidos </button>
+      </section>
+      <section className='dashboardSection'>
+        <section className='menuSection'>
+          <section className='selectSection'>
+            <select data-testid='table' className='tableSelect'>
+              <option value="">Mesa</option>
+              <option value="mesa1">Mesa1</option>
+              <option value="mesa2">Mesa2</option>
+              <option value="mesa3">Mesa3</option>
+              <option value="mesa4">Mesa4</option>
+              <option value="mesa5">Mesa5</option>
+              <option value="mesa6">Mesa6</option>
+              <option value="mesa7">Mesa7</option>
+              <option value="mesa8">Mesa8</option>
+              <option value="mesa9">Mesa9</option>
+              <option value="mesa10">Mesa10</option>
+            </select>
+          </section>
+          <section className='buttonMenuSection'>
+            <button className='menuButton' id='breakfast'>
+              Desayuno
+            </button>
+            <button className='menuButton' id='lunch'>
+              Almuerzo/cena
+            </button>
+          </section>
+          <section className='foodSection'>
+            {/* aquí va la informacion del menú que se renderiza */}
+          </section>
+        </section>
+        <section className='orderInfoSection'>
+          <h3> Pedido </h3>
+          <table>
+            <td> Producto </td>
+            <td> Cantidad </td>
+            <td> Valor </td>
+            <tr></tr>
+          </table>
+          <button> Enviar pedido </button>
+        </section>
+      </section>
+    </section>
+  )
 }
 
 export default NewOrder;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import '../src/components/Login/login.css'
+import '../src/components/Waiter/NewOrder.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
