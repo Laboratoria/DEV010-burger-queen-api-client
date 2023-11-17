b7aa17949f059087935cae3426c5a87964694835
n-rogget <nicole.rogget@gmail.com>
2023-11-17 11:19:56 -0300

    test de historia 1 corriendo al 80%

--- src/components/Login/login.test.tsx
+++ src/components/Login/login.test.tsx
@@ -32,30 +32,81 @@ describe('Login component', () => {
       expect(auth).toHaveBeenCalledWith(email, password);
     });
   });
+ 
 
-  it('should display error message for missing email and password', async () => {
-    //console.log(auth)
-
+  it('should display error message for incorrect password case', async () => {
     (auth as jest.Mock).mockResolvedValueOnce('Incorrect password')
-
-
     const { getByText, getByPlaceholderText } = render(<Login />);
-
-    // Simular el ingreso de datos incorrectos
     fireEvent.change(getByPlaceholderText('Email'), {
       target: { value: 'test@example.com' },
     });
     fireEvent.change(getByPlaceholderText('Contraseña'), {
       target: { value: 'incorrectPassword' },
     });
-
-    // Simular el clic en el botón de ingresar sin proporcionar email y contraseña
     fireEvent.click(getByText('Ingresar'));
-
     // Esperar a que se renderice el mensaje de error
     await waitFor(() => {
       expect(getByText('Contraseña incorrecta')).toBeInTheDocument();
     });
   });
-
+  it('should display error message for cannot find user case', async () => {
+    (auth as jest.Mock).mockResolvedValueOnce('Cannot find user')
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    fireEvent.change(getByPlaceholderText('Email'), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(getByPlaceholderText('Contraseña'), {
+      target: { value: 'correctPassword' },
+    });
+    fireEvent.click(getByText('Ingresar'));
+    // Esperar a que se renderice el mensaje de error
+    await waitFor(() => {
+      expect(getByText('Usuario no encontrado')).toBeInTheDocument();
+    });
+  });
+  it('should display error message for email and password are required case', async () => {
+    (auth as jest.Mock).mockResolvedValueOnce('Email and password are required')
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    fireEvent.change(getByPlaceholderText('Email'), {
+      target: { value: '' },
+    });
+    fireEvent.change(getByPlaceholderText('Contraseña'), {
+      target: { value: '' },
+    });
+    fireEvent.click(getByText('Ingresar'));
+    // Esperar a que se renderice el mensaje de error
+    await waitFor(() => {
+      expect(getByText('Ingrese email y contraseña')).toBeInTheDocument();
+    });
+  });
+  it('should display error message for email format is invalid case', async () => {
+    (auth as jest.Mock).mockResolvedValueOnce('Email format is invalid')
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    fireEvent.change(getByPlaceholderText('Email'), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(getByPlaceholderText('Contraseña'), {
+      target: { value: 'correctPassword' },
+    });
+    fireEvent.click(getByText('Ingresar'));
+    // Esperar a que se renderice el mensaje de error
+    await waitFor(() => {
+      expect(getByText('Formato de email incorrecto')).toBeInTheDocument();
+    });
+  });
+  it('should display error message for email format is invalid case', async () => {
+    (auth as jest.Mock).mockResolvedValueOnce('')
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    fireEvent.change(getByPlaceholderText('Email'), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(getByPlaceholderText('Contraseña'), {
+      target: { value: 'correctPassword' },
+    });
+    fireEvent.click(getByText('Ingresar'));
+    // Esperar a que se renderice el mensaje de error
+    await waitFor(() => {
+      expect(getByText('Error durante la autenticación. Por favor, inténtalo de nuevo.')).toBeInTheDocument();
+    });
+  });
 });
