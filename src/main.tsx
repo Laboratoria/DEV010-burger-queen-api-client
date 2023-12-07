fe8d3914ed2be828ced0b74607b5de4f191a0599
unknown <ginapedraza00@gmail.com>
2023-12-07 12:09:29 -0300

    Mejorado el css

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import '../src/components/Login/login.css'
-import '../src/components/Waiter/NewOrder.css'
+import '../src/components/Waiter/newOrder.css'
 import '../src/components/Chef/ChefOrders.css'
 import '../src/components/Waiter/OrderList.css'
+import '../src/components/Header/Header.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
