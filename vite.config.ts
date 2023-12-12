e556d50a46b7f1b3bfdd4281f0c8580a3b851400
Nicole Rogget <nicole.rogget@gmail.com>
2023-12-12 12:31:38 -0300

    Se cambia configuracion de html..

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,27 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }) as unknown as import('vite').Plugin,
+    VitePWA({ manifest:{
+      display: 'standalone',
+      display_override: ['window-controls-overlay'],
+      lang: 'es-ES',
+      name: "Burger Queen",
+      description: "La app sirve para ayudar a los trabajadores de un local de hamburguesas a tomar y ordenar pedidos",
+      theme_color: "#F5F5F5",
+      background_color: "#FEAE0D",
+      icons: [
+        {
+          "src": "/public/half-logo.png",
+          "sizes": "120x120",
+          "type": "image/png"
+        },
+        {
+          "src": "public/burger-queen-logo-background.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    } }) as unknown as import('vite').Plugin,
   ],
 });
