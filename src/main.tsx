03dc5e6879e8e30bcd72cd7773e5dbc067a92741
Nicole Rogget <nicole.rogget@gmail.com>
2023-12-12 18:06:35 -0300

    Se eliminan arregla error de importacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import "../src/components/Login/login.css";
 import "../src/components/Waiter/NewOrder.css";
 import "../src/components/Chef/ChefOrders.css";
